fad07bf050c6cd5b9915c7ff289ceefd649883d4
Andrey Talantsev <slivtime@gmail.com>
2020-06-30 23:54:57 +0300

    Remove redundant check

--- src/1-two-sum/two-sum.go
+++ src/1-two-sum/two-sum.go
@@ -3,12 +3,8 @@ package twoSum
 func twoSum(nums []int, target int) []int {
 	for i, val := range nums {
 		for j := i + 1; j < len(nums); j++ {
-			sum := val + nums[j]
-			if sum == target {
+			if val+nums[j] == target {
 				return []int{i, j}
-			}
-			if sum > target {
-				continue
 			}
 		}
 	}
