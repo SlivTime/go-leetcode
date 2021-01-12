d2defe72c761e2e67e79d4c53cb8e60ce88e8d51
SlivTime <slivtime@gmail.com>
2021-01-12 14:30:39 +0300

    Better solution for Add Two Numbers

--- src/2-add-two-numbers/add-two-numbers.go
+++ src/2-add-two-numbers/add-two-numbers.go
@@ -5,84 +5,31 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func listToInt(l *ListNode) int {
-	result := 0
-	multiplier := 1
-	current := l
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var carry, value int
+	result := &ListNode{}
+	current := result
 
-	for {
-		if current == nil {
-			break
+	for l1 != nil || l2 != nil {
+		value = carry
+		if l1 != nil {
+			value += l1.Val
+			l1 = l1.Next
 		}
-		result += current.Val * multiplier
-		multiplier *= 10
+		if l2 != nil {
+			value += l2.Val
+			l2 = l2.Next
+		}
+		carry = value / 10
+		value = value % 10
+
+		current.Next = &ListNode{value, nil}
+
 		current = current.Next
 	}
-	return result
-}
 
-func intToList(n int) *ListNode {
-	start := ListNode{}
-	current := &start
-	for n > 0 {
-		nodeVal := n % 10
-		current.Val = nodeVal
-		n = n / 10
-		if n > 0 {
-			next := ListNode{}
-			current.Next = &next
-			current = &next
-		}
+	if carry > 0 {
+		current.Next = &ListNode{carry, nil}
 	}
-	return &start
-}
-
-// Simpler version with just adding two ints with some type conversions
-// It is not universal due to int overflow, but it is more easy to read
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return intToList(listToInt(l1) + listToInt(l2))
-}
-
-// Common version with iteration over two linked lists
-// Only this one passes the leetcode tests
-func addTwoNumbersLoop(l1 *ListNode, l2 *ListNode) *ListNode {
-	carryBit := 0
-	result := ListNode{}
-	current := &result
-	currentL1 := l1
-	currentL2 := l2
-	for {
-		if currentL1 == nil && currentL2 == nil {
-			// Terminate only if both lists are empty
-			if carryBit == 1 {
-				current.Next = &ListNode{carryBit, nil}
-			}
-			break
-		}
-		if currentL1 == nil {
-			// Create virtual zero-value Node
-			currentL1 = &ListNode{}
-		}
-		if currentL2 == nil {
-			// Create virtual zero-value Node
-			currentL2 = &ListNode{}
-		}
-		sum := currentL1.Val + currentL2.Val + carryBit
-		carryBit = 0
-		if sum > 9 {
-			carryBit = 1
-		}
-		current.Val = sum % 10
-
-		currentL1 = currentL1.Next
-		currentL2 = currentL2.Next
-
-		if currentL1 != nil || currentL2 != nil {
-			next := ListNode{}
-			current.Next = &next
-			current = &next
-		}
-	}
-
-	return &result
+	return result.Next
 }
